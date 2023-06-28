2d4963eb155cd43fcff9c207289a6b35e585740d
nanqiang <nanqianglin@gmail.com>
2023-06-28 21:04:29 +0800

    feat: add react-ui package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,14 @@
 import { Button, Header } from "ui";
+import { Footer, Header as MyHeader } from "@nankio/react-ui";
 
 export default function Page() {
   return (
     <>
       <Header text="Web" />
       <Button />
+      <p>ppp</p>
+      <MyHeader />
+      <Footer />
     </>
   );
 }
